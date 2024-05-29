b333700c9105c8afa7fd4c69cd6a2478fc5fd6fd
jh0222 <gihyuny0222@gmail.com>
2024-05-29 14:13:51 +0900

    first commit

---
+++ app/(movies)/movies/[id]/loading.tsx
@@ -0,0 +1,7 @@
+async function getVideos(id: string) {
+  0 
+}
+
+export default function Loading() {
+  return <h2>Loading a movie :id</h2>
+}
---
+++ app/about-us/company/jobs/sales/page.tsx
@@ -0,0 +1,9 @@
+
+
+export default function Page() {
+  return (
+    <div>
+      <h1>Sales jobs</h1>
+    </div>
+  )
+}
